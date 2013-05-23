01b4b69de2d03e739be1af22ab1a2906d364e148
rao xunrong <raoxunrong@gmail.com>
2013-05-23 18:04:43 +0800

    raoxunrong, add test for JavascriptGenerator

--- src/test/java/org/raoxunrong/check/JavascriptGeneratorTest.java
+++ src/test/java/org/raoxunrong/check/JavascriptGeneratorTest.java
@@ -2,6 +2,9 @@ package org.raoxunrong.check;
 
 import org.junit.Test;
 
+import static org.apache.commons.lang3.StringUtils.contains;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.raoxunrong.check.JavascriptGenerator.generateDispatchEventScript;
 
 public class JavascriptGeneratorTest {
@@ -10,7 +13,13 @@ public class JavascriptGeneratorTest {
     public void shouldGenerateDispatchEventScript() {
         String dispatchEventScript = generateDispatchEventScript("elementId", "eventId");
 
-
+        assertThat(contains(dispatchEventScript, "var evt = document.createEvent(\"Events\");"), is(true));
+        assertThat(contains(dispatchEventScript, "evt.initEvent(\"eventId\", true, false);"), is(true));
+        assertThat(contains(dispatchEventScript, "var element = document.getElementById(\"elementId\");"), is(true));
+        assertThat(contains(dispatchEventScript, "element.dispatchEvent(evt);"), is(true));
     }
+
+
+
 
 }
