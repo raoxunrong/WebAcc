221fd9d738de8521c3a54ef2c054a467bf0700be
shaneke <shaneke2010@gmail.com>
2013-05-24 17:10:02 +0800

    Add unit test for PlainTextDictionary.addNewWord()

--- src/test/java/org/raoxunrong/check/PlainTextDictionaryTest.java
+++ src/test/java/org/raoxunrong/check/PlainTextDictionaryTest.java
@@ -3,7 +3,7 @@ package org.raoxunrong.check;
 import org.junit.Test;
 import org.raoxunrong.check.spellcheck.PlainTextDictionary;
 
-import java.util.HashSet;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PlainTextDictionaryTest {
@@ -42,6 +42,20 @@ public class PlainTextDictionaryTest {
         String filePath = this.getClass().getClassLoader().getResource(PLAINTEXT_DIC_RESOURCE_NAME).getFile();
         PlainTextDictionary plainTextDictionary = new PlainTextDictionary(filePath, "\n");
         String[] words = new String[]{"google", "baidu", "tw"};
+        for(String word: words){
+            assertTrue(plainTextDictionary.isWord(word));
+        }
+    }
+
+    @Test
+    public void shouldAddNewWord(){
+        PlainTextDictionary plainTextDictionary = new PlainTextDictionary(PLAINTEXT_DIC_RESOURCE_NAME);
+        String[] words = new String[]{"new", "nothing"};
+        for(String word: words){
+            assertFalse(plainTextDictionary.isWord(word));
+            plainTextDictionary.addNewWord(word);
+        }
+
         for(String word: words){
             assertTrue(plainTextDictionary.isWord(word));
         }
