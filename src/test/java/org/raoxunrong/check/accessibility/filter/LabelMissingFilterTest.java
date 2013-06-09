8ac61e966c5ab6ce65d2a04432bb201d83f82341
rao xunrong <raoxunrong@gmail.com>
2013-06-09 16:09:22 +0800

    raoxunrong, fix test

--- src/test/java/org/raoxunrong/check/accessibility/filter/LabelMissingFilterTest.java
+++ src/test/java/org/raoxunrong/check/accessibility/filter/LabelMissingFilterTest.java
@@ -24,40 +24,14 @@ public class LabelMissingFilterTest extends AbstractHtmlFilterTest {
     @Test
     public void shouldFilterEmptyElements() throws IOException, URISyntaxException {
         StringBuffer elementBuffer = new StringBuffer();
-        elementBuffer.append("<input />").append("<h2></h2>").append("<h3></h3>")
-                .append("<h4></h4>").append("<h5></h5>").append("<h6></h6>")
-                .append("<table><th></th></table>").append("<a href=\"123\"></a>");
+        elementBuffer.append("<input/>").append("<input type='checkbox'/>").append("<input type='password'/>")
+                .append("<input type='radio'/>").append("<input type='text'/>").append("<input type=''/>");
 
         setBodyResource(elementBuffer.toString());
         writeHtmlResource();
         openTestPage();
 
         Collection<WebElement> filterElements = labelMissingFilter.filter(getCheckablePage());
-        assertThat(filterElements.size(), is(8));
-
-        Iterator<WebElement> iterator = filterElements.iterator();
-        WebElement webElement = iterator.next();
-        assertThat(webElement.getTagName(), is("h1"));
-
-        webElement = iterator.next();
-        assertThat(webElement.getTagName(), is("h2"));
-
-        webElement = iterator.next();
-        assertThat(webElement.getTagName(), is("h3"));
-
-        webElement = iterator.next();
-        assertThat(webElement.getTagName(), is("h4"));
-
-        webElement = iterator.next();
-        assertThat(webElement.getTagName(), is("h5"));
-
-        webElement = iterator.next();
-        assertThat(webElement.getTagName(), is("h6"));
-
-        webElement = iterator.next();
-        assertThat(webElement.getTagName(), is("th"));
-
-        webElement = iterator.next();
-        assertThat(webElement.getTagName(), is("a"));
+        assertThat(filterElements.size(), is(6));
     }
 }
